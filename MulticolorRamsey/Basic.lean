ebf2d406747eb17631c6cbc1d34045890a299347
ovi <ovi@posteo.de>
2025-01-29 13:56:40 +0100

    adding a lemma signature

--- MulticolorRamsey/Basic.lean
+++ MulticolorRamsey/Basic.lean
@@ -1,5 +1,9 @@
 import Mathlib.Combinatorics.SimpleGraph.Basic
 
+import Mathlib.Probability.Distributions.Uniform
+
+open MeasureTheory ProbabilityTheory
+
 namespace SimpleGraph
 
 variable {V : Type α} (G : SimpleGraph V)
@@ -8,3 +12,17 @@ variable {V : Type α} (G : SimpleGraph V)
 abbrev EdgeColoring (C : Type) := G.edgeSet → C
 
 end SimpleGraph
+
+
+variable {Ω : Type*} [MeasurableSpace Ω] {P : Measure Ω} [IsProbabilityMeasure P]
+
+variable (n r : ℕ)
+
+lemma moments (X : Type) [Fintype X] [Nonempty X] [MeasureSpace X]
+    (U U' : Ω → X) (_ : IndepFun U U' P)
+    (u : P.map U =  (PMF.uniformOfFintype X).toMeasure)
+    (u' : P.map U' = (PMF.uniformOfFintype X).toMeasure)
+    (σ : Fin r → (X → (Fin n → ℝ))) (l : Fin r → ℕ)
+    (i : Fin r)
+    : 0 ≤ 𝔼[ fun (x, x') => ∏ i, (dotProduct (σ i x) (σ i x')) ^ (l i) ] :=
+  sorry
